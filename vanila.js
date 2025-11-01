41283e375192277b660aefd7ddf157e837a06fb4
AgungPrawoto13 <agungprawoto997@gmail.com>
2025-11-01 20:52:34 +0700

    make desaign mobile

--- vanila.js
+++ vanila.js
@@ -1,9 +1,16 @@
 function checkNavBarState() {
     const myElement = document.getElementById('navigation');
+    const myDesktop = document.getElementById('desktop');
+    const myMobile = document.getElementById('mobile');
 
     if (window.matchMedia("(max-width: 768px)").matches){
-        // mobile display
+        // for mobile
         myElement.classList.add('fs-1');
+        myMobile.classList.add('show');
+
+    } else {
+        // for desktop
+        myDesktop.classList.add('show');
     }
 }
 
